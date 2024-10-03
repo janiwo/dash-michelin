5a856c695b44835e4a12e63ef7b0535d1f3127c9
Felipe Forero <felipe.forerome@gmail.com>
2024-10-03 23:28:36 +0200

    Moved style to css file

--- app/layouts/buttons_overlay/buttons_overlay.py
+++ app/layouts/buttons_overlay/buttons_overlay.py
@@ -5,10 +5,5 @@ from components.button import button
 
 buttons_overlay = html.Div(
     children=[button("Filter"), button("Dashbard"), button("List")],
-    style={
-        "position": "absolute",
-        "top": "9vh",
-        "left": "10px",
-        "zIndex": "1000",
-    },
+    className="buttons-overlay",
 )
