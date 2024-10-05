3a5ceb971be24e4601ba1e9fb74c7f8bc4feb413
janiwo13 <jan.niklas.wolters@gmail.com>
2024-10-05 19:06:19 +0200

    implemented map clustering, changed default view

--- app/layouts/map_graph/map_graph.py
+++ app/layouts/map_graph/map_graph.py
@@ -1,6 +1,7 @@
 # from app.assets.data import data
 import plotly.express as px
 from dash import dcc
+from assets.colors import Colors
 from assets.data import data
 
 df = data.df
@@ -11,13 +12,16 @@ map_fig = px.scatter_map(
     lat=cols.norm.latitude,
     lon=cols.norm.longitude,
     color=cols.code.award_stars_count,
-    size=cols.code.award_stars_count,
+    # size=cols.code.award_stars_count,
     color_continuous_scale=px.colors.sequential.Viridis,
     size_max=15,
-    zoom=10,
+    zoom=2,
+    center=dict(lat=20, lon=0),
 )
-map_fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-map_fig.update_layout(coloraxis_showscale=False)
+map_fig.update_traces(cluster=dict(enabled=True, color=Colors.light_grey, maxzoom=3))
+map_fig.update_layout(
+    margin={"r": 0, "t": 0, "l": 0, "b": 0}, coloraxis_showscale=False
+)
 
 
 map_graph = dcc.Graph(
