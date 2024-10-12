f0e364693209c9282f7467e4d6c66bf2b5c3bf96
Felipe Forero <felipe.forerome@gmail.com>
2024-10-12 13:45:30 +0200

    Button overlays have icons instead of text

--- app/components/button.py
+++ app/components/button.py
@@ -1,18 +1,29 @@
 from dash_bootstrap_components import Button
+from dash import html
 
 
 class ButtonComponent:
     def __init__(
-        self, label: str, id: str = None, color: str = "light", className: str = "me-1"
+        self,
+        label: str,
+        icon: str = None,
+        id: str = None,
+        color: str = "light",
+        className: str = "me-1",
     ):
         self.label = label
+        self.icon = icon
         self.id = id if id else "btn-" + self.clean_string(label)
         self.color = color
         self.className = f"button-component {className}"
 
     def render(self) -> Button:
         return Button(
-            self.label, id=self.id, color=self.color, className=self.className
+            html.I(className=self.icon) if self.icon else self.label,
+            id=self.id,
+            color=self.color,
+            className=self.className,
+            style={"margin": "0.75vh"},
         )
 
     @staticmethod
--- app/layouts/buttons_overlay/buttons_overlay.py
+++ app/layouts/buttons_overlay/buttons_overlay.py
@@ -1,13 +1,15 @@
 from dash import html
 
+import dash_html_components as html
 from components.button import ButtonComponent
 
 
 buttons_overlay = html.Div(
     children=[
-        ButtonComponent("Filter").render(),
-        ButtonComponent("Dashboard").render(),
-        ButtonComponent("List").render(),
+        ButtonComponent("Filter", icon="bi bi-funnel").render(),
+        ButtonComponent("Dashboard", icon="bi bi-bar-chart-line-fill").render(),
+        ButtonComponent("List", icon="bi bi-card-list").render(),
     ],
     className="buttons-overlay",
+    style={"display": "flex", "flexDirection": "column"},
 )
