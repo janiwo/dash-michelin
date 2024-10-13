3fc88bcc20ccb3645b8404ad9ce1644d15b22e99
Felipe Forero <felipe.forerome@gmail.com>
2024-10-13 22:29:39 +0200

    Refactored restaurant_bar

--- app/layouts/layout.py
+++ app/layouts/layout.py
@@ -4,7 +4,7 @@ from layouts.navbar.navbar import navbar
 from layouts.buttons_overlay.buttons_overlay import buttons_overlay
 from layouts.map_graph.map_graph import map_graph
 from layouts.profile_modal.profile_modal import profile_modal
-from layouts.restaurant_list.restaurant_list import restaurant_list
+from layouts.restaurant_bar.restaurant_bar import restaurant_bar
 from layouts.dashboard.dashboard import dashboard
 
 layout = html.Div(
@@ -12,7 +12,7 @@ layout = html.Div(
         dcc.Store(id="side-bar-visibility", data=True),
         navbar,
         buttons_overlay,
-        restaurant_list,
+        restaurant_bar,
         map_graph,
         profile_modal,
         dashboard,
--- app/layouts/restaurant_bar/restaurant_bar.py
+++ app/layouts/restaurant_bar/restaurant_bar.py
@@ -6,7 +6,7 @@ from components.side_bar import SideBar
 
 test_list = data.df.iloc[0:5, :].to_dict("records")
 
-restaurant_list = SideBar(
+restaurant_bar = SideBar(
     children=SideBarList(
         data=data, restaurant_ids=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     ).render(),
