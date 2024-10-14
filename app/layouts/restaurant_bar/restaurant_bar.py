d367323f65c340219bceeed4ca07eebf9abbe723
Felipe Forero <felipe.forerome@gmail.com>
2024-10-14 22:48:55 +0200

    Solved Merge

--- app/layouts/restaurant_bar/restaurant_bar.py
+++ app/layouts/restaurant_bar/restaurant_bar.py
@@ -1,13 +1,13 @@
 import dash_bootstrap_components as dbc
 from dash import html
-from components.side_bar_list import SideBarList
+from components.restaurant_bar_list import RestaurantBarList
 from assets.data import data
 from components.side_bar import SideBar
 
 test_list = data.df.iloc[0:5, :].to_dict("records")
 
 restaurant_bar = SideBar(
-    children=SideBarList(
+    children=RestaurantBarList(
         data=data, restaurant_ids=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     ).render(),
     id="restaurant-list",
