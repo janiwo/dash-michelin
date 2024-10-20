0fd4f92fe7ac023b9bf654511e3f8d151dd6c462
janiwo13 <jan.niklas.wolters@gmail.com>
2024-10-20 17:32:56 +0200

    added custom bootstrap stylesheet

--- app/layouts/navbar/navbar.py
+++ app/layouts/navbar/navbar.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 navbar = dbc.NavbarSimple(
     brand="Dash Michelin",
     brand_href="#",
-    color="#BB2631",
+    color="primary",
     dark=True,
     class_name="navbar",
 )
